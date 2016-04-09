34cb3f69dfa74ac38c1d1ac34de2806d06520db1
Matthew Faigan <matthew.faigan@gmail.com>
2016-04-09 15:49:07 -0400

    Username prompt and chunk size calculations

--- finder.py
+++ finder.py
@@ -1,1 +1,29 @@
+import math
+import re
+import sys
 
+
+with open("roommembers.txt", "r") as f:
+    # turn roommembers.txt into list of names
+    member_list = f.read().split(", ")
+
+
+def main():
+    # prompt for username
+    username = input("Enter your username: ")
+    if username in member_list:
+        pass
+    else:
+        print("Error: username was not in room")
+        input("Press Enter to exit...")
+        sys.exit()
+
+
+def find_name_slice(fullname):
+    # size of name chunk is: length of name / total people in room (5295)
+    chunk_size = math.ceil(len(fullname) / 5295)
+    if chunk_size < 2:
+        chunk_size = 2
+
+if __name__ == "__main__":
+    main()
