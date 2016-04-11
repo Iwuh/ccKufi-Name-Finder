5add54b192e4091a4f3284abddd8735bc9848bc2
Matthew Faigan <matthew.faigan@gmail.com>
2016-04-11 19:14:49 -0400

    Finished name chunk calculations

--- finder.py
+++ finder.py
@@ -12,18 +12,28 @@ def main():
     # prompt for username
     username = input("Enter your username: ")
     if username in member_list:
-        pass
+        name_chunk = find_name_chunk(username, member_list.index(username))
     else:
         print("Error: username was not in room")
         input("Press Enter to exit...")
         sys.exit()
 
 
-def find_name_slice(fullname):
-    # size of name chunk is: length of name / total people in room (5295)
+def find_name_chunk(fullname, username_index):
+    # size of name chunk is: length of name / total people in room (5295), rounded up
     chunk_size = math.ceil(len(fullname) / 5295)
+    # minimum chunk size is 2
     if chunk_size < 2:
         chunk_size = 2
+
+    # position where slice begins is: (position of name in user list / total people in room ) * length of username
+    # note: reddit's servers were f**d around the time ccKufi was created, so your username piece may be different
+    # from what this method says. In the future there will be a long search option that uses every possible slice of
+    # your username.
+    chunk_begin = math.ceil((username_index / 5295 * len(fullname)))
+    chunk = fullname[chunk_begin:chunk_size]
+
+    return chunk
 
 if __name__ == "__main__":
     main()
