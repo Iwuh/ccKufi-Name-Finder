f4ff60c02564648f927b38220ee9b78619429ad9
Matthew Faigan <matthew.faigan@gmail.com>
2016-04-15 12:36:01 -0400

    Fix search method, add colorama import

--- finder.py
+++ finder.py
@@ -1,7 +1,6 @@
 import math
 import re
-import sys
-
+import colorama
 
 with open("roommembers.txt", "r") as f:
     # turn roommembers.txt into list of names
@@ -13,10 +12,10 @@ def main():
     username = input("Enter your username: ")
     if username in member_list:
         name_chunk = find_name_chunk(username, member_list.index(username))
+        character_list = search_name_chunk(name_chunk, member_list.index(username))
     else:
         print("Error: username was not in room")
         input("Press Enter to exit...")
-        sys.exit()
 
 
 def find_name_chunk(fullname, username_index):
@@ -40,17 +39,25 @@ def search_name_chunk(search_term, username_index):
     s = re.compile('(%s)' % search_term)
     with open("roomname.txt", "r") as r:
         # approximate beginning of name chunk in room name
-        # multiplied by 2 because every username got 2 letters in ccKufi's name
+        # multiplied by 2 because most if not everyone got 2 letters in ccKufi's name and I don't know a better solution
         center_search = r.read()[username_index * 2]
         # give an extra 2000 characters on each side to be safe (full name is >10000 characters long)
-        search_section = r.read()[center_search-2000:center_search+2000]
+        if center_search-2000 < 0:
+            # however, we don't want the start to be lower than 0
+            search_section = r.read()[0:center_search + 2000]
+        else:
+            search_section = r.read()[center_search - 2000:center_search + 2000]
         # use list comprehension to get a list of tuples where matches were found
         result_list = [m.span() for m in s.finditer(search_section)]
         adjusted_list = []
-        # reduce second value of every tuple in result_list by 1
-        # however, tuples are immutable so we need to create new ones...
+        # convert list of tuples to list of character indices where matches were found
         for i in result_list:
-            adjusted_list.append((i[0], i[1]-1))
+            adjusted_list.append(i[0])
+            adjusted_list.append(i[1]-1)
+
+        return adjusted_list
 
 if __name__ == "__main__":
+    colorama.init()
     main()
+    colorama.deinit()
